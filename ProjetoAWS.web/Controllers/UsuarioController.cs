c067f1443ed68b739cd81fc886f0322f49fb69e3
Argollo <rafaelargollo22@hotmail.com>
2022-07-14 21:34:22 -0300

    Implementando API Cadastrar Imagem

--- ProjetoAWS.web/Controllers/UsuarioController.cs
+++ ProjetoAWS.web/Controllers/UsuarioController.cs
@@ -2,6 +2,10 @@ using Microsoft.AspNetCore.Mvc;
 using ProjetoAWS.lib.Data.Interfaces;
 using ProjetoAWS.lib.Models;
 using ProjetoAWS.web.DTos;
+using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+using Amazon.S3;
+using Amazon.S3.Model;
 
 namespace ProjetoAWS.web.Controllers
 {
@@ -10,9 +14,14 @@ namespace ProjetoAWS.web.Controllers
     public class UsuarioController : ControllerBase
     {
         private readonly IUsuarioRepositorio _repositorio;
-        public UsuarioController(IUsuarioRepositorio repositorio)
+        private readonly AmazonRekognitionClient _rekognitionClient;
+        private readonly IAmazonS3 _amazonS3;
+        private static readonly List<string> _extensoesImagem = new List<string>() { "image/jpeg", "image/png" };
+        public UsuarioController(IUsuarioRepositorio repositorio, AmazonRekognitionClient client, IAmazonS3 amazonS3)
         {
             _repositorio = repositorio;
+            _rekognitionClient = client;
+            _amazonS3 = amazonS3;
         }
 
         [HttpGet]
@@ -22,7 +31,7 @@ namespace ProjetoAWS.web.Controllers
             return Ok(resposta);
         }
 
-        [HttpPost]
+        [HttpPost("AddUsuario")]
         public async Task<IActionResult> AddUsuario(UsuarioDTO usuarioDTO)
         {
             var usuario = new Usuario(usuarioDTO.Id, usuarioDTO.Email, usuarioDTO.Cpf, usuarioDTO.DataNascimento, usuarioDTO.Nome, usuarioDTO.Senha, usuarioDTO.DataCriacao);
@@ -42,6 +51,69 @@ namespace ProjetoAWS.web.Controllers
         {
             await _repositorio.DeletarAsync(id);
             return Ok();
+        }
+
+        [HttpPost("CadastrarImagem")]
+        public async Task<IActionResult> CadastrarImagem(int id, IFormFile imagem)
+        {
+            var fotoUsuario = await AddImagem(imagem);
+            await AnalisarRosto(imagem.FileName);
+            await _repositorio.AtualizarUrlFotoCadastro(id, imagem.FileName);
+            return Ok("Imagem cadastrada");
+        }
+
+        private async Task<IActionResult> AddImagem(IFormFile imagem)
+        {
+            if (!_extensoesImagem.Contains(imagem.ContentType))
+                return BadRequest("Tipo Invalido");
+            using (var streamDaImagem = new MemoryStream())
+            {
+                await imagem.CopyToAsync(streamDaImagem);
+
+                var request = new PutObjectRequest();
+                request.Key = imagem.FileName;
+                request.BucketName = "imagens-teste-dodev";
+                request.InputStream = streamDaImagem;
+
+                var resposta = await _amazonS3.PutObjectAsync(request);
+                return Ok(resposta);
+            }
+        }
+
+        private async Task<IActionResult> AnalisarRosto(string nomeArquivo)
+        {
+            var entrada = new DetectFacesRequest();
+            var imagem = new Image();
+
+            var s3Object = new Amazon.Rekognition.Model.S3Object()
+            {
+                Bucket = "imagens-teste-dodev",
+                Name = nomeArquivo
+            };
+
+            imagem.S3Object = s3Object;
+            entrada.Image = imagem;
+            entrada.Attributes = new List<string>() { "ALL" };
+
+            var resposta = await _rekognitionClient.DetectFacesAsync(entrada);
+
+            if (resposta.FaceDetails.Count == 1 && resposta.FaceDetails.First().Eyeglasses.Value == false)
+            {
+                return Ok(resposta);
+            }
+            else
+            {
+                await _amazonS3.DeleteObjectAsync("imagens-teste-dodev", nomeArquivo);
+                return BadRequest("Imagem inválida");
+            }
+
+        }
+
+        [HttpDelete("DeletarImagem")]
+        public async Task<IActionResult> DeletarImagem(string nomeArquivo)
+        {
+            await _amazonS3.DeleteObjectAsync("imagens-teste-dodev", nomeArquivo);
+            return Ok("Imagem deletada");
         }
     }
 }
