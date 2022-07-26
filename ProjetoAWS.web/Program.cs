5ed54dc7051168b1d23a96ef4d321de35310b39b
Argollo <rafaelargollo22@hotmail.com>
2022-07-25 21:38:39 -0300

    Retirando injeções de dependencia da program

--- ProjetoAWS.web/Program.cs
+++ ProjetoAWS.web/Program.cs
@@ -1,11 +1,4 @@
-using Amazon.Rekognition;
-using Amazon.Runtime;
-using Amazon.S3;
-using Microsoft.EntityFrameworkCore;
-using ProjetoAWS.Application.Services;
-using ProjetoAWS.lib.Data;
-using ProjetoAWS.lib.Data.Interfaces;
-using ProjetoAWS.lib.Data.Repositorios;
+using ProjetoAWS.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,21 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddAWSService<IAmazonS3>();
-builder.Services.AddScoped<AmazonRekognitionClient>();
-
-builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
-builder.Services.AddScoped<IUsuarioApplication, UsuarioApplication>();
-
-builder.Services.AddDbContext<AWSContext>(conn =>
-conn.UseNpgsql(builder.Configuration.GetConnectionString("AWSDB"))
-.UseSnakeCaseNamingConvention());
-
-var awsOptions = builder.Configuration.GetAWSOptions();
-awsOptions.Credentials = new EnvironmentVariablesAWSCredentials();
-builder.Services.AddDefaultAWSOptions(awsOptions);
-
+builder.Services.AddConfig(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
